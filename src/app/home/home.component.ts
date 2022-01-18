1818bbfac32ed896acada63098c4882f90a404da
AndreaQuagliana <andrea.quagliana@s2e-mail.com>
2022-01-18 14:09:11 +0100

    sort not completed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,19 +11,19 @@ export class HomeComponent implements OnInit {
 
   @ViewChild(MatTable) table: any;
 
-  displayedColumns: string[] = ['name', 'Description', 'FileType', 'Download'];
+  displayedColumns: string[] = ['CompanyName', 'Description', 'FileType', 'Download'];
 
   dataSource: any[] = [
-    { name: 'Hydrogen', Description: 1111, FileType: 'txt', Download:  "download" },
-    { name: 'Helium', Description: 4.0026, FileType: 'txt',Download: "download"},
-    { name: 'Lithium', Description: 6.941, FileType: 'txt',Download: "download"},
-    { name: 'Beryllium', Description: 9.0122, FileType: 'txt',Download: "download"},
-    { name: 'Boron', Description: 10.811, FileType: 'txt',Download: "download"},
-    { name: 'Carbon', Description: 12.0107, FileType: 'txt',Download: "download"},
-    { name: 'Nitrogen', Description: 14.0067, FileType: 'txt',Download: "download"},
-    { name: 'Oxygen', Description: 15.9994, FileType: 'txt',Download: "download"},
-    { name: 'Fluorine', Description: 18.9984, FileType: 'txt',Download: "download"},
-    { name: 'Neon', Description: 20.1797, FileType: 'txt',Download: "download"},
+    { CompanyName: 'Hydrogen', Description: 1111, FileType: 'txt', Download:  "download" },
+    { CompanyName: 'Helium', Description: 4.0026, FileType: 'txt',Download: "download"},
+    { CompanyName: 'Lithium', Description: 6.941, FileType: 'txt',Download: "download"},
+    { CompanyName: 'Beryllium', Description: 9.0122, FileType: 'txt',Download: "download"},
+    { CompanyName: 'Boron', Description: 10.811, FileType: 'txt',Download: "download"},
+    { CompanyName: 'Carbon', Description: 12.0107, FileType: 'txt',Download: "download"},
+    { CompanyName: 'Nitrogen', Description: 14.0067, FileType: 'txt',Download: "download"},
+    { CompanyName: 'Oxygen', Description: 15.9994, FileType: 'txt',Download: "download"},
+    { CompanyName: 'Fluorine', Description: 18.9984, FileType: 'txt',Download: "download"},
+    { CompanyName: 'Neon', Description: 20.1797, FileType: 'txt',Download: "download"},
   ];
 
   isSearching: boolean = false
@@ -31,18 +31,6 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-
-  // aggiunge una riga editabile alla tabella dei reperibili
-  addRow(){
-    this.dataSource.push({
-      name:"",
-      Description: "",
-      FyleType: "",
-      Download: ""
-    })
-    this.table.renderRows();
   }
 
   switchSearch(){
