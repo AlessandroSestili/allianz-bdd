3e0c918e725090b18a3aced8edd365605b7dddc9
AndreaQuagliana <andrea.quagliana@s2e-mail.com>
2022-01-18 12:36:31 +0100

    addRows

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatButton } from '@angular/material/button';
 import { MatTable } from '@angular/material/table';
 
 @Component({
@@ -13,22 +14,40 @@ export class HomeComponent implements OnInit {
   displayedColumns: string[] = ['name', 'Description', 'FileType', 'Download'];
 
   dataSource: any[] = [
-    { name: 'Hydrogen', Decription: 1.0079, FileType: 'H', Download: false},
-    { name: 'Helium', Description: 4.0026, FileType: 'He',Download: false},
-    { name: 'Lithium', Description: 6.941, FileType: 'Li',Download: false},
-    { name: 'Beryllium', Description: 9.0122, FileType: 'Be',Download: false},
-    { name: 'Boron', Description: 10.811, FileType: 'B',Download: false},
-    { name: 'Carbon', Description: 12.0107, FileType: 'C',Download: false},
-    { name: 'Nitrogen', Description: 14.0067, FileType: 'N',Download: false},
-    { name: 'Oxygen', Description: 15.9994, FileType: 'O',Download: false},
-    { name: 'Fluorine', Description: 18.9984, FileType: 'F',Download: false},
-    { name: 'Neon', Description: 20.1797, FileType: 'Ne',Download: false},
+    { name: 'Hydrogen', Decription: 1.0079, FileType: 'txt', Download:  "ok" },
+    { name: 'Helium', Description: 4.0026, FileType: 'txt',Download: "false"},
+    { name: 'Lithium', Description: 6.941, FileType: 'txt',Download: "false"},
+    { name: 'Beryllium', Description: 9.0122, FileType: 'txt',Download: "false"},
+    { name: 'Boron', Description: 10.811, FileType: 'txt',Download: "false"},
+    { name: 'Carbon', Description: 12.0107, FileType: 'txt',Download: "false"},
+    { name: 'Nitrogen', Description: 14.0067, FileType: 'txt',Download: "false"},
+    { name: 'Oxygen', Description: 15.9994, FileType: 'txt',Download: "false"},
+    { name: 'Fluorine', Description: 18.9984, FileType: 'txt',Download: "false"},
+    { name: 'Neon', Description: 20.1797, FileType: 'txt',Download: "false"},
   ];
+
+  isSearching: boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
-    // this.table.renderRows();
+    this.table.renderRows();
+  }
+
+
+  // aggiunge una riga editabile alla tabella dei reperibili
+  addRow(){
+    this.dataSource.push({
+      name:"",
+      Description: "",
+      FyleType: "",
+      Download: ""
+    })
+    this.table.renderRows();
+  }
+
+  switchSearch(){
+    this.isSearching = !this.isSearching
   }
 
 }
